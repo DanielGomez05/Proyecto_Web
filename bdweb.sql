874004e55149dc623a83447fd1e1089b2f0c9539
NEMO42227 <84051038+NEMO42227@users.noreply.github.com>
2023-06-18 16:47:25 -0600

    Add files via upload

---
+++ bdweb.sql
@@ -0,0 +1,51 @@
+CREATE TABLE Usuario (
+  ID_Usuario INT PRIMARY KEY,
+  Nombre VARCHAR(50),
+  Apellido VARCHAR(50),
+  CorreoElectronico VARCHAR(50) unique,
+  Contraseña VARCHAR(50),
+  telefono int(12) unique,
+  rol_id INT
+);
+drop table usuario;
+CREATE TABLE Producto (
+  ID_Producto INT PRIMARY KEY,
+  Tipo ENUM('Papeleria', 'Merceria'),
+  Nombre VARCHAR(50),
+   Precio DECIMAL(10, 2) ,
+  Categoria VARCHAR(50),
+  Descripcion VARCHAR(100)
+  
+);
+DROP TABLE Producto;
+CREATE TABLE Pedido (
+  ID_Pedido INT PRIMARY KEY,
+  ID_Producto INT  unique,
+  Fecha DATE,
+  Total DECIMAL(10, 2),
+  Estado ENUM('pendiente', 'en proceso', 'enviado', 'entregado', 'cancelado'),
+  ID_Usuario INT,
+  FOREIGN KEY (ID_Usuario) REFERENCES Usuario(ID_Usuario),
+  FOREIGN KEY (ID_Producto) REFERENCES Producto(ID_Producto)
+);
+DROP TABLE Pedido;
+
+drop table Usuario_Producto_Deseado;
+CREATE TABLE Usuario_Producto_Deseado (
+  ID_Usuario INT,
+  ID_Producto INT,
+  FOREIGN KEY (ID_Usuario) REFERENCES Usuario(ID_Usuario),
+  FOREIGN KEY (ID_Producto) REFERENCES Producto(ID_Producto)
+);
+drop table Usuario_Producto_Comprado;
+CREATE TABLE Usuario_Producto_Comprado (
+  ID_Usuario INT,
+  ID_Producto INT,
+  fecha date,
+  FOREIGN KEY (ID_Usuario) REFERENCES Usuario(ID_Usuario),
+  FOREIGN KEY (ID_Producto) REFERENCES Producto(ID_Producto)
+);
+
+
+
+
